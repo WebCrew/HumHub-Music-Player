fd45aeda6dfeb7064040db7854a8394cc5c073db
WebCrew <palareas@gmail.com>
2017-01-12 14:46:37 +0100

    Create musicplayer.php

---
+++ widgets/views/musicplayer.php
@@ -0,0 +1,17 @@
+<?php
+use yii\helpers\Html;
+?>
+
+<div class="panel panel-default" id="music-player-panel">
+    <?php echo \humhub\widgets\PanelMenu::widget(array('id' => 'music-player-panel')); ?>
+
+    <div class="panel-heading">
+        <?php echo $title; ?>
+    </div>
+    <div class="panel-body">
+    
+        \\ aplitudejs code here
+        
+        <?php endforeach; ?>
+    </div>
+</div>
