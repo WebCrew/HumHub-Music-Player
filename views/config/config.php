1d83414bc10046c731535f910f7b983dba4b9c2b
WebCrew <palareas@gmail.com>
2017-01-12 14:54:00 +0100

    Create config.php
    integrated config file

---
+++ views/config/config.php
@@ -0,0 +1,32 @@
+<?php
+use yii\helpers\Url;
+use yii\helpers\Html;
+use humhub\compat\CActiveForm;
+?>
+
+<div class="panel panel-default">
+    <div class="panel-heading"><?php echo Yii::t('MusicPlayerModule.base', '<strong>Music Player</strong> Module Configuration'); ?></div>
+    <div class="panel-body">
+
+        <br>
+
+        <?php $form = CActiveForm::begin(); ?>
+        <?php echo $form->errorSummary($model); ?>
+
+        <div class="form-group">
+            <?php echo $form->labelEx($model, 'panelTitle'); ?>
+            <?php echo $form->textField($model, 'panelTitle', array('class' => 'form-control')); ?>
+            <?php echo $form->error($model, 'panelTitle'); ?>
+        </div>
+
+        <hr>
+        <?php echo Html::submitButton(Yii::t('MusicPlayerModule.base', 'Save'), array('class' => 'btn btn-primary')); ?>
+        <a class="btn btn-default"
+           href="<?php echo Url::to(['/admin/module']); ?>"><?php echo Yii::t('MusicPlayerModule.base', 'Back to modules'); ?></a>
+
+        <!-- show flash message after saving -->
+        <?php echo humhub\widgets\DataSaved::widget(); ?>
+
+        <?php CActiveForm::end(); ?>
+    </div>
+</div>
