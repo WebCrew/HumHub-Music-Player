35863f6229981d508768e5ba7e76958606c12165
WebCrew <palareas@gmail.com>
2017-01-12 14:29:12 +0100

    Update Module.php
    fixed some issues

--- Module.php
+++ Module.php
@@ -1,44 +1,22 @@
 <?php
-
 namespace humhub\modules\musicplayer;
-
-use humhub\models\Setting;
-use Yii;
 use yii\helpers\Url;
-
-class Module extends \humhub\components\Module {
-
-  const NAME = 'musicplayer';
-
-  /**
-   * On build of the dashboard sidebar widget, add the music player widget if module is enabled.
-   *
-   * @param type $event
-   */
-  public static function onSidebarInit($event) {
-    if (Yii::$app->hasModule(self::NAME)) {
-
-      $event->sender->addWidget(
-        widgets\Sidebar::className(),
-        [],
-        [
-          'sortOrder' => 201,
-        ]
-      );
+use humhub\models\Setting;
+use humhub\modules\musicplayer\widgets\MusicPlayerSidebarWidget;
+class Module extends \humhub\components\Module
+{
+    public static function onSidebarInit($event)
+    {
+        $event->sender->addWidget(MusicPlayerSidebarWidget::className(), array(), array('sortOrder' => 250));
     }
-  }
-
-  public function getConfigUrl() {
-    return Url::to(['/musicplayer/config/config']);
-  }
-
-  /**
-   * Enables this module
-   */
-  public function enable() {
-    if (!Yii::$app->hasModule(self::NAME)) {
-      parent::enable();
-  }
+    public function getConfigUrl()
+    {
+        return Url::to(['/musicplayer/config/config']);
+    }
+    public function enable()
+    {
+        parent::enable();
+        Setting::Set('panelTitle', 'Music Player', 'musicplayer');
+    }
 }
-
 ?>
