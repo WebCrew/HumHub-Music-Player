e7be468109f388fa34aed703377ce4ff2fd36c96
WebCrew <palareas@gmail.com>
2017-01-12 14:24:20 +0100

    Update config.php
    did some corrections

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ return [
   'id'        => 'musicplayer',
   'class'     => 'humhub\modules\musicplayer\Module',
   'namespace' => 'humhub\modules\musicplayer',
-    'events' => [
-        ['class' => Sidebar::className(), 'event' => Sidebar::EVENT_INIT, 'callback' => ['humhub\modules\musicplayer\Module', 'onSidebarInit']],
-    ],
+    'events' => array(
+        array('class' => Sidebar::className(), 'event' => Sidebar::EVENT_INIT, 'callback' => array('humhub\modules\musicplayer\Module', 'onSidebarInit')),
+    ),
 ];
 ?>
