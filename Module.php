cdec940d55dd012a857eca5ec1293c891daf02e5
WebCrew <palareas@gmail.com>
2017-01-16 16:36:38 +0100

    new

--- Module.php
+++ Module.php
@@ -1,22 +1,7 @@
 <?php
 namespace humhub\modules\musicplayer;
-use yii\helpers\Url;
-use humhub\models\Setting;
-use humhub\modules\musicplayer\widgets\MusicPlayerSidebarWidget;
-class Module extends \humhub\components\Module
+
+class Module extends \humhub\modules\content\components\ContentContainerModule
 {
-    public static function onSidebarInit($event)
-    {
-        $event->sender->addWidget(MusicPlayerSidebarWidget::className(), array(), array('sortOrder' => 250));
-    }
-    public function getConfigUrl()
-    {
-        return Url::to(['/musicplayer/config/config']);
-    }
-    public function enable()
-    {
-        parent::enable();
-        Setting::Set('panelTitle', 'Music Player', 'musicplayer');
-    }
 }
-?>
+
