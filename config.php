58f4e0219cf1966b8b7a502597e5856859fe3010
WebCrew <palareas@gmail.com>
2017-01-16 16:35:49 +0100

    new

--- config.php
+++ config.php
@@ -1,13 +1,43 @@
 <?php
-
-use humhub\modules\dashboard\widgets\Sidebar;
+namespace humhub\modules\musicplayer;
 
 return [
-  'id'        => 'musicplayer',
-  'class'     => 'humhub\modules\musicplayer\Module',
-  'namespace' => 'humhub\modules\musicplayer',
-    'events' => array(
-        array('class' => Sidebar::className(), 'event' => Sidebar::EVENT_INIT, 'callback' => array('humhub\modules\musicplayer\Module', 'onSidebarInit')),
-    ),
+    'id' => 'musicplayer',
+    'class' => 'humhub\modules\musicplayer\Module',
+    'namespace' => 'humhub\modules\musicplayer',
+    'events' => [
+        [
+            'class' => \humhub\modules\dashboard\widgets\Sidebar::className(),
+            'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
+            'callback' => array(
+                'humhub\modules\musicplayer\Events',
+                'addMusicplayerFrame'
+            )
+        ],
+        [
+            'class' => \humhub\modules\space\widgets\Sidebar::className(),
+            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
+            'callback' => array(
+                'humhub\modules\musicplayer\Events',
+                'addMusicplayerFrame'
+            )
+        ],
+        [
+            'class' => \humhub\modules\user\widgets\ProfileSidebar::className(),
+            'event' => \humhub\modules\user\widgets\ProfileSidebar::EVENT_INIT,
+            'callback' => array(
+                'humhub\modules\musicplayer\Events',
+                'addMusicplayerFrame'
+            )
+        ],
+        [
+            'class' => \humhub\modules\admin\widgets\AdminMenu::className(),
+            'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
+            'callback' => [
+                'humhub\modules\musicplayer\Events',
+                'onAdminMenuInit'
+            ]
+        ]
+    ]
 ];
 ?>
