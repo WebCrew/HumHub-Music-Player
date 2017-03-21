3ccb4228df0dfbafcd5494e3ba4b90855bf65781
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-20 20:03:30 -0400

    Update base.php

--- messages/en_uk/base.php
+++ messages/en_uk/base.php
@@ -1,6 +1,9 @@
 <?php
 return [
+    '<strong>Music Player</strong>' => '<strong>Music Player</strong>',
     'Musicplayer' => 'Music Player',
     'sort' => 'Musicplayer sort order',
+    'eg:  "99999999"' => 'eg:  "99999999"',
+    'Widget positioning' => 'Widget positioning',
     'save' => 'Save',
 ];
