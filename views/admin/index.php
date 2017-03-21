cd2a8171c6737706dca8ef9ae2e0744a25b05c6b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-03-20 20:03:17 -0400

    Update index.php

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,22 +1,26 @@
 <?php
-use humhub\compat\CActiveForm;
-use humhub\compat\CHtml;
+
+use yii
+use yii\bootstrap\ActiveForm;
+use yii\helper\Html;
 use humhub\models\Setting;
 use humhub\modules\musicplayer\controllers\AdminController;
+
 ?>
+
 <div class="panel panel-default">
-	<div class="panel-heading"><?=Yii::t('MusicplayerModule.base', '<strong>Music Player</strong>'); ?></div>
+	<div class="panel-heading"><?= Yii::t('MusicplayerModule.base', '<strong>Music Player</strong>'); ?></div>
 	<div class="panel-body">
-		<?php $form = CActiveForm::begin(['id' => 'musicplayer-settings-form']); ?>
-			<?=$form->errorSummary($model); ?>
-			<p class="help-block"><?=Yii::t('MusicplayerModule.base', 'eg:  "99999999"'); ?></p>
+		<?php $form = ActiveForm::begin(['id' => 'musicplayer-settings-form']); ?>
+			<?= $form->errorSummary($model); ?>
+			<p class="help-block"><?= Yii::t('MusicplayerModule.base', 'eg:  "99999999"'); ?></p>
 			<div class="form-group">
-				<?=$form->labelEx($model, 'sort'); ?>
-				<?=$form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'musicplayer')]); ?>
+				<?= $form->labelEx($model, 'sort'); ?>
+				<?= $form->textField($model, 'sort', ['class' => 'form-control', 'readonly' => Setting::IsFixed('sort', 'musicplayer')]); ?>
 			</div>
 			<p class="help-block"><?=Yii::t('MusicplayerModule.base', 'Widget positioning.'); ?></p>
-			<?= CHtml::submitButton(Yii::t('MusicplayerModule.base', 'save'), ['class' => 'btn btn-primary']); ?>
-			<?=\humhub\widgets\DataSaved::widget(); ?>
-		<?php CActiveForm::end(); ?>
+			<?= Html::submitButton(Yii::t('MusicplayerModule.base', 'Save'), ['class' => 'btn btn-primary']); ?>
+			<?= \humhub\widgets\DataSaved::widget(); ?>
+		<?php ActiveForm::end(); ?>
 	</div>
 </div>
